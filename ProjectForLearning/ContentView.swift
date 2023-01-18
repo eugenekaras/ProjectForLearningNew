d496371510f03acd3bc28fc6a35abb27365d22b0
ekaras <key.cpp@gmail.com>
2023-01-18 23:43:33 +0100

    Firebase crashlytics

--- ProjectForLearning/ContentView.swift
+++ ProjectForLearning/ContentView.swift
@@ -10,10 +10,14 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundColor(.accentColor)
+//            Text("Hello, world!")
+            
+            Button("Crash") {
+              fatalError("Crash was triggered")
+            }
         }
         .padding()
     }
--- ProjectForLearning/ProjectForLearningApp.swift
+++ ProjectForLearning/ProjectForLearningApp.swift
@@ -6,9 +6,28 @@
 //
 
 import SwiftUI
+import Firebase
+
+final class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+//    print("test")
+    FirebaseApp.configure()
+      
+    return true
+  }
+}
 
 @main
 struct ProjectForLearningApp: App {
+
+//
+//    init() {
+//      FirebaseApp.configure()
+//    }
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
