7d2d07ce3b7bdfed3da08fc973390550bac045d7
ekaras <key.cpp@gmail.com>
2023-01-18 12:21:43 +0100

    start

---
+++ ProjectForLearning/ProjectForLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectForLearningApp.swift
+//  ProjectForLearning
+//
+//  Created by Евгений Карась on 18.01.23.
+//
+
+import SwiftUI
+
+@main
+struct ProjectForLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
